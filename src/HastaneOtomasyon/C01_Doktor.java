6c1206c078d7c1d49f51918e8b6749de8eca08ee
firdevs yildiz <firdevsomeryildiz@gmail.com>
2022-06-03 12:46:39 +0100

    03.06.22

--- src/HastaneOtomasyon/C01_Doktor.java
+++ src/HastaneOtomasyon/C01_Doktor.java
@@ -1,5 +1,28 @@
 package HastaneOtomasyon;
 
 public class C01_Doktor {
-    
+    private String isim;
+    private String soyIsim;
+    private String unvan;
+
+
+    public String getIsim() {
+        return isim;
+    }
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+    public String getSoyIsim() {
+        return soyIsim;
+    }
+    public void setSoyIsim(String soyIsim) {
+        this.soyIsim = soyIsim;
+    }
+    public String getUnvan() {
+        return unvan;
+    }
+    public void setUnvan(String unvan) {
+        this.unvan = unvan;
+    }
+
 }
